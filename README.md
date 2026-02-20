8d8c2ef53bf0879318ceea252eea10eadf2e8cda
venomkage <ceomindset3@gmail.com>
2026-02-20 22:19:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# sboot-simple-real-time-chat-app
+
+I made this while I am learning Spring Boot so please don't expect too much from it.
+It's a simple websocket real-time chat app. 
+You can open two browser window, give sender name and start chatting.
+<br>
+
+Of course, you'll need to first clone this and run this on your local machine and then
+<br>
+open localhost:8080
---
+++ src/main/java/com/example/demo/controllers/ChatController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+
+    @GetMapping
+    public String chat(){
+        return "chat";
+    }
+
+}
